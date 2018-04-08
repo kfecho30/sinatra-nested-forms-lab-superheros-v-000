1135cdf8e87b3f3282f4d74f606b4b5db2dfa71b
Kelsey Fecho <kfecho30@gmail.com>
2018-04-08 18:28:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      binding.pry
       erb :index
     end
 
     post '/team' do
+      binding.pry
       erb :team
     end
 end
